095f9239fa62cc96d01a6f60d01af177145f9373
Samuel Biazotto <samuel.biazotto@hotmail.com>
2021-05-17 00:14:02 -0300

    spark clusterization  working in docker with others tools

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 SPARK_VERSION="3.0.0"
 HADOOP_VERSION="2.7"
+JUPYTERLAB_VERSION="2.1.5"
 ELASTIC_VERSION="7.12.0"
-# JUPYTERLAB_VERSION="2.1.5"
 
 # -- Building the Images
 
@@ -14,20 +14,19 @@ docker build \
 docker build \
   --build-arg spark_version="${SPARK_VERSION}" \
   --build-arg hadoop_version="${HADOOP_VERSION}" \
-  --build-arg elastic_version="${ELASTIC_VERSION}" \
   -f spark-base.Dockerfile \
   -t spark-base .
 
-# docker build \
-#   -f spark-master.Dockerfile \
-#   -t spark-master .
+docker build \
+  -f spark-master.Dockerfile \
+  -t spark-master .
 
-# docker build \
-#   -f spark-worker.Dockerfile \
-#   -t spark-worker .
+docker build \
+  -f spark-worker.Dockerfile \
+  -t spark-worker .
 
-# docker build \
-#   --build-arg spark_version="${SPARK_VERSION}" \
-#   --build-arg jupyterlab_version="${JUPYTERLAB_VERSION}" \
-#   -f jupyterlab.Dockerfile \
-#   -t jupyterlab .
+docker build \
+  --build-arg spark_version="${SPARK_VERSION}" \
+  --build-arg jupyterlab_version="${JUPYTERLAB_VERSION}" \
+  -f jupyterlab.Dockerfile \
+  -t jupyterlab .
